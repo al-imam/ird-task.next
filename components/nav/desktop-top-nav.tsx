9f1c44484ba36a941e60026e0fc261c1c35367fb
al-imam <alimam01828@gmail.com>
2024-01-31 00:53:07 +0600

    create desktop nav

--- components/nav/desktop-top-nav.tsx
+++ components/nav/desktop-top-nav.tsx
@@ -1,16 +1,23 @@
+import profileIcon from "$assets/icons/profile.svg";
+import rectAngle from "$assets/icons/rectangle.svg";
+import { SearchGlobal } from "$components/utils";
 import { cn } from "$shadcn/utils";
+import Image from "next/image";
 
 interface DesktopTopNavProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {}
 
 export function DesktopTopNav({ className, ...rest }: DesktopTopNavProps) {
   return (
-    <nav className={cn("flex items-center justify-between rounded-3xl", className)} {...rest}>
-      <h2 className="text-2xl font-semibold text-foreground">Dua Page</h2>
-      <div>
-        <input type="text" />
-      </div>
+    <nav className={cn("flex items-center rounded-3xl", className)} {...rest}>
+      <h2 className="mr-auto text-2xl font-semibold text-foreground">Dua Page</h2>
+      <SearchGlobal placeholder="Search by dua name" />
 
-      <div></div>
+      <div className="h-full w-[calc(var(--layout-gap)+270px)]">
+        <button className="ml-auto flex items-center gap-1 rounded-md ">
+          <Image src={profileIcon} alt="profile" />
+          <Image src={rectAngle} alt="arrow" className="h-3 w-3" />
+        </button>
+      </div>
     </nav>
   );
 }
---
+++ components/utils/search.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import searchIcon from "$assets/icons/search.svg";
+import Image from "next/image";
+import { forwardRef, useId, useState } from "react";
+import { cn } from "shadcn/lib/utils";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const SearchCategory = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const id = useId();
+
+  return (
+    <label
+      htmlFor={id}
+      className={cn(
+        "group flex h-full gap-1.5 rounded-md border-none bg-background text-sm outline outline-1 outline-border",
+        { "outline-2 outline-primary": isFocused },
+        className
+      )}
+    >
+      <div className="m-1 flex cursor-pointer items-center justify-center rounded-sm px-2 focus-visible:outline-none">
+        <Image src={searchIcon} alt="search" />
+      </div>
+      <input
+        id={id}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        type={type}
+        className={cn("focus-visible: !p-4 !pr-0 [all:unset]  group-hover:cursor-text", { "": isFocused })}
+        ref={ref}
+        {...props}
+      />
+    </label>
+  );
+});
+
+const SearchGlobal = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const id = useId();
+
+  return (
+    <label
+      htmlFor={id}
+      className={cn(
+        "group flex h-full gap-1.5 rounded-md border-none bg-background text-sm outline outline-1 outline-border",
+        { "outline-2 outline-primary": isFocused },
+        className
+      )}
+    >
+      <input
+        id={id}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        type={type}
+        className={cn("focus-visible: !p-4 !pr-0 [all:unset]  group-hover:cursor-text", { "": isFocused })}
+        ref={ref}
+        {...props}
+      />
+      <button className="m-1 ml-0 flex cursor-pointer items-center justify-center rounded-sm bg-muted px-3 focus-visible:outline-none">
+        <Image src={searchIcon} alt="search" />
+      </button>
+    </label>
+  );
+});
+
+SearchCategory.displayName = "search-category";
+SearchGlobal.displayName = "search-global";
+
+export { SearchCategory, SearchGlobal };
