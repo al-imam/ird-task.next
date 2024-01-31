e4f922a0b1f8246a807a703272fe143bcb3d07bd
al-imam <alimam01828@gmail.com>
2024-01-31 22:14:42 +0600

    show correct dua

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,23 +1,33 @@
 import { DuaCard } from "$components/dua-card";
 import { ScrollArea } from "$shadcn/ui/scroll-area";
+import { Category } from "$types";
+import { joinUrl } from "$util";
+import axios from "axios";
+import { redirect } from "next/navigation";
+import { Fragment } from "react";
 
-export default async function DuaPage({ ...rest }) {
-  // eslint-disable-next-line no-console
-  console.log(rest);
+export default async function DuaPage({ searchParams }: { searchParams: Record<string, string> }) {
+  const cat = parseInt(searchParams.cat ?? "", 10);
+  if (isNaN(cat)) redirect("/");
+
+  const { data } = await axios.get<Category[]>(joinUrl(process.env.NEXT_PUBLIC_API_URL, "/dua/category", cat));
+  const subCats = data.map(c => c.sub_categories).flat(1);
 
   return (
     <ScrollArea className="">
       <main className="flex flex-col gap-3 pb-[--padding-edge]">
-        <div className="rounded-[0.625rem] border bg-background px-8 py-4">
-          <p className="text-base">
-            <span className="font-medium text-primary">Section:</span> The servant is dependent on his Lord
-          </p>
-        </div>
-        <DuaCard />
-        <DuaCard />
-        <DuaCard />
-        <DuaCard />
-        <DuaCard />
+        {subCats.map(subCat => (
+          <Fragment key={subCat.id + subCat.cat_id + subCat.subcat_id}>
+            <div className="rounded-[0.625rem] border bg-background px-8 py-4">
+              <p className="text-base">
+                <span className="font-medium text-primary">Section:</span> {subCat.subcat_name_en}
+              </p>
+            </div>
+            {subCat.duas.map(dua => (
+              <DuaCard dua={dua} key={dua.id + dua.dua_id} />
+            ))}
+          </Fragment>
+        ))}
       </main>
     </ScrollArea>
   );
--- components/dua-card.tsx
+++ components/dua-card.tsx
@@ -5,54 +5,48 @@ import planIcon from "$assets/icons/plan.svg";
 import playButtonIcon from "$assets/icons/play-button.svg";
 import reportIcon from "$assets/icons/report.svg";
 import shareIcon from "$assets/icons/share.svg";
+import { Dua } from "$types";
 import Image from "next/image";
 
-export function DuaCard() {
+export function DuaCard({ dua }: { dua: Dua }) {
   return (
     <div className="flex flex-col gap-7 rounded-[0.625rem] border bg-background px-8 py-4">
       <p className="flex items-center gap-2.5 text-base font-medium text-primary">
-        <Image src={duaCardIcon} alt="allah" /> <span>2. Conditions for Dua to be successful</span>
+        <Image src={duaCardIcon} alt="allah" /> <span>{dua.dua_name_en}</span>
       </p>
 
-      <p className="text-base text-foreground">
-        Prophet (ﷺ) used to say after every compulsory prayer, The servant will ask his Lord for all of his religiously
-        and worldly needs, because the treasure of all things is in the hands of Allah. Allah says (interpretation of
-        the meaning): “And there is not a thing but that with Us are its depositories, and We do not send it down except
-        according to a known measure.” (Sura Al-Hijr 15:21) No one can withhold what Allah gives; And, no one can give
-        what he resists.
-      </p>
+      {dua.top_en && <p className="text-base text-foreground">{dua.top_en}</p>}
+      {dua.dua_arabic && <p className="py-2.5 text-right font-quran text-2xl text-foreground">{dua.dua_arabic}</p>}
+      {(dua.translation_en || dua.transliteration_en) && (
+        <div className="space-y-2.5">
+          {dua.transliteration_en && (
+            <p>
+              <span className="font-semibold">Transliteration:</span> {dua.transliteration_en}
+            </p>
+          )}
+          {dua.translation_en && (
+            <p>
+              <span className="font-semibold"> Translation:</span> {dua.translation_en}
+            </p>
+          )}
+        </div>
+      )}
 
-      <p className="font-quran text-right text-2xl text-foreground">
-        لَا إِلَهَ إِلَّا اللَّهُ وَحْدَهُ لَا شَرِيْكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ
-        قَدِيرٌ، اَللَّهُمَّ لَا مَانِعَ لِمَا أَعْطَيْتَ وَلَا مُعْطِيَ لِمَا مَنَعْتَ وَلَا يَنْفَعُ ذَا الْجَدِّ
-        مِنْكَ الْجَدُّ
-      </p>
+      {dua.refference_en && (
+        <div className="">
+          <p className="font-semibold text-primary">Reference:</p>
+          <p className="font-semibold">{dua.refference_en}</p>
+        </div>
+      )}
 
-      <div className="space-y-2.5">
-        <p>
-          <span className="font-semibold">Transliteration:</span> Laa ilaaha illallahu wahdahu laa sharika lahu,
-          lahul-mulku wa lahul-hamdu wa huwa &apos;alaa kulli shay&apos;in qadir. Allaahumma laa maani&apos;a limaa
-          a&apos;taita wa laa mu&apos;tia limaa mana&apos;ta wa laa yanfa&apos;u dhal-jaddi minka al-jaddu
-        </p>
-        <p>
-          <span className="font-semibold"> Translation:</span> There is none worthy of worship except Allah alone with
-          no partner or associate. He is the Dominion and to Him be all praise, and He is able to do all things. O
-          Allah, one can withhold what You have given and none can give what You have withheld, and no wealth or fortune
-          can benefit anyone for from You comes all wealth and fortune.
-        </p>
-      </div>
+      <div className="flex items-center">
+        {dua.audio && (
+          <button className="mr-auto rounded-full">
+            <Image src={playButtonIcon} alt="play-audio" />
+          </button>
+        )}
 
-      <div className="">
-        <p className="font-semibold text-primary">Reference:</p>
-        <p className="font-semibold">Bukhari: 844</p>
-      </div>
-
-      <div className="flex items-center justify-between">
-        <button className="rounded-full">
-          <Image src={playButtonIcon} alt="play-audio" />
-        </button>
-
-        <div className="flex h-full items-center justify-between gap-8">
+        <div className="ml-auto flex h-full items-center justify-between gap-8">
           <button className="transition-transform hover:scale-105 focus-visible:ring-0">
             <Image src={copyIcon} alt="copy" />
           </button>
