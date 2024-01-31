1ec7c8f5affab7a8044ce1e48d26fff81301f942
al-imam <alimam01828@gmail.com>
2024-01-31 21:23:26 +0600

    add fallback for undefined cat search query

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,36 @@
+import { Category } from "$types";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import slugify from "slugify";
+import { joinUrl } from "./util/joinUrl";
 
-export function middleware(request: NextRequest) {
-  // Improvement - fetch first category
-  return NextResponse.redirect(new URL("/dua's-importance?cat=1", request.url));
+async function getFirstCategory() {
+  const res = await fetch(joinUrl(process.env.NEXT_PUBLIC_API_URL, "dua", "first-category"), { method: "GET" });
+  return (await res.json()) as Category;
+}
+
+export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  if (/^\/[^/]+$/.test(path) || path === "/") {
+    if (path === "/") {
+      const category = await getFirstCategory();
+      return NextResponse.redirect(
+        new URL(`/${slugify(category.cat_name_en, { lower: true })}?cat=${category.cat_id}`, request.url)
+      );
+    }
+
+    const cat = parseInt(request.nextUrl.searchParams.get("cat") ?? "", 10);
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(cat)) {
+      const category = await getFirstCategory();
+      return NextResponse.redirect(
+        new URL(`/${slugify(category.cat_name_en, { lower: true })}?cat=${category.cat_id}`, request.url)
+      );
+    }
+  }
 }
 
 export const config = {
-  matcher: "/",
+  matcher: ["/((?!api|_next/static|_next/image|favicon).*)"],
 };
