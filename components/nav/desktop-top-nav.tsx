ad7253d40892afc73585a8843e5869771257cf5f
al-imam <alimam01828@gmail.com>
2024-02-01 01:37:02 +0600

    fix top nav

--- components/nav/desktop-top-nav.tsx
+++ components/nav/desktop-top-nav.tsx
@@ -12,7 +12,7 @@ export function DesktopTopNav({ className, ...rest }: DesktopTopNavProps) {
       <h2 className="mr-auto text-2xl font-semibold text-foreground">Dua Page</h2>
       <SearchGlobal placeholder="Search by dua name" />
 
-      <div className="h-full w-[calc(var(--layout-gap)+270px)]">
+      <div className="h-full w-[calc(var(--layout-gap)+330px)]">
         <button className="ml-auto flex items-center gap-1 rounded-md ">
           <Image src={profileIcon} alt="profile" />
           <Image src={rectAngle} alt="arrow" className="h-3 w-3" />
