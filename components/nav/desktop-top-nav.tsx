389258f91bdc99a3c2f0f45900991d6409a54784
al-imam <alimam01828@gmail.com>
2024-02-02 00:18:34 +0600

    create profile model

--- components/nav/desktop-top-nav.tsx
+++ components/nav/desktop-top-nav.tsx
@@ -1,18 +1,39 @@
 "use client";
 
+import aboutIcon from "$assets/icons/about.svg";
+import copyrightIcon from "$assets/icons/copyright.svg";
+import creditIcon from "$assets/icons/credit.svg";
+import downloadIcon from "$assets/icons/download.svg";
 import duaLogo from "$assets/icons/dua-logo.svg";
 import openLogo from "$assets/icons/open.svg";
+import privacyIcon from "$assets/icons/privacy.svg";
 import profileIcon from "$assets/icons/profile.svg";
+import projectsIcon from "$assets/icons/projects.svg";
 import rectAngle from "$assets/icons/rectangle.svg";
 import settingIcon from "$assets/icons/settings.svg";
+import supportIcon from "$assets/icons/support.svg";
+
 import { Settings } from "$components/settings";
 import { SearchGlobal } from "$components/utils";
+import { Popover, PopoverContent } from "$shadcn/ui/popover";
 import { Sheet, SheetContent, SheetTrigger } from "$shadcn/ui/sheet";
 import { cn } from "$shadcn/utils";
 import { Navigation } from "$types";
+import { PopoverTrigger } from "@radix-ui/react-popover";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import { Categories } from "./categories";
+
+const items = [
+  { text: "Support Us", icon: supportIcon },
+  { text: "Download Dua App", icon: downloadIcon },
+  { text: "Privacy Policy", icon: privacyIcon },
+  { text: "Thanks & Credits", icon: creditIcon },
+  { text: "About Us", icon: aboutIcon },
+  { text: "Copyright Warning", icon: copyrightIcon },
+  { text: "Our Other Projects", icon: projectsIcon },
+];
 
 interface DesktopTopNavProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
   navigation: Navigation[];
@@ -58,11 +79,34 @@ export function DesktopTopNav({ className, navigation, ...rest }: DesktopTopNavP
       </Sheet>
 
       <div className="h-full 2xl:w-[calc(var(--layout-gap)+330px)]">
-        <button className="ml-auto flex items-center gap-1 rounded-md ">
-          <Image src={profileIcon} alt="profile" />
-          <Image src={rectAngle} alt="arrow" className="h-3 w-3" />
-        </button>
+        <Popover>
+          <PopoverTrigger asChild>
+            <button className="ml-auto flex items-center gap-1 rounded-md ">
+              <Image src={profileIcon} alt="profile" />
+              <Image src={rectAngle} alt="arrow" className="h-3 w-3" />
+            </button>
+          </PopoverTrigger>
+          <PopoverContent align="end" side="bottom">
+            <div className="space-y-1">
+              {items.map(item => (
+                <ProfileItem key={item.text} text={item.text} icon={item.icon} />
+              ))}
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
     </nav>
+  );
+}
+
+export function ProfileItem({ text, icon }: { text: string; icon: any }) {
+  return (
+    <Link
+      href="#"
+      className="flex cursor-pointer items-center gap-2 rounded-md px-3 py-2.5 transition duration-150 ease-in-out hover:bg-muted"
+    >
+      <Image className="flex h-5 w-5 items-center justify-center object-contain text-white" src={icon} alt={"link"} />
+      <span className="text-sm text-foreground">{text}</span>
+    </Link>
   );
 }
